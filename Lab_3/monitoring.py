572c610d11de8520f11d82e24b5a5f688daa1a02
Yurii Urshanskyi <59485534+yuriiurshanskyi@users.noreply.github.com>
2020-11-21 16:18:38 +0200

    added Lab_3

---
+++ Lab_3/monitoring.py
@@ -0,0 +1,32 @@
+import requests
+import json
+import logging
+import time
+
+logging.basicConfig(
+    filename="server.log",
+    filemode='a',
+    level=logging.INFO,
+    format='{levelname} {asctime} {name} : {message}',
+    style='{'
+)
+log = logging.getLogger(__name__)
+
+
+def main(url):
+    while True:
+        try:
+            r = requests.get(url)
+            data = json.loads(r.content)
+            logging.info("Сервер доступний. Час на сервері: %s", data['date'])
+            logging.info("Запитувана сторінка: : %s", data['current_page'])
+            logging.info("Відповідь сервера містить наступні поля:")
+            for key in data.keys():
+                logging.info("Ключ: %s, Значення: %s", key, data[key])
+        except Exception as e:
+            logging.error(f'Сталась помилка: {e}')
+        time.sleep(60)
+
+
+if __name__ == '__main__':
+    main("http://localhost:8000/health")
